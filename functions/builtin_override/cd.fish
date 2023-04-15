e5bb8330ab5d512ab533ba9b3a04cf65781b114f
Luu Hoang Kien <kien.luuhoang.arch@gmail.com>
2023-04-16 03:57:06 +0700

    Improve cd

--- functions/builtin_override/cd.fish
+++ functions/builtin_override/cd.fish
@@ -1,36 +1,39 @@
-function __cd_explicit_path --description "Find explicit path"
-  string length -q "$argv"; or set argv "$HOME"
-  set argv (realpath $argv)
-  echo $argv
+function __cd_explicit_path
+  test -z $argv && echo $HOME || echo (realpath $argv)
 end
 
-function __cd_remove_existed --description "Remove an existed item from a list"
-  set -l remove_item  "$argv[1]"
-  set -l from_list    "$argv[2]"
-  set -l tmp_list
-
-  set from_list (echo $from_list | sed "s`[[:space:]]\+` `g")
-  for item in (string split ' ' $from_list)
-    if not test $item = $remove_item
-      set -a tmp_list $item
-    end
-  end
-  echo "$tmp_list"
+function __cd_int_check
+  string match -qr '^\s*[0-9]+' "$argv" && return 0 || return 1
 end
 
-function __cd_int_check --description "Check a variable is an integer or not"
-  if test (count $argv) -gt 1
-    echo "__cd_int_check: Superfluous argument"
-    return 1
-  end
-  string match -qr '^[0-9]+' "$argv"; and return 0; or return 1
+function log
+  echo $argv >> $HOME/cd.log
+end
+
+function __cd_add_history
+  set -l new_path       "$argv[1]"
+  set -l old_history    (string split --no-empty ' ' "$argv[2]")
+
+  # Remove existed path in history
+  set -l new_history    (string match -v $new_path $old_history)
+
+  # Remove oldest item in history if OOM
+  # Not implemented yet
+  # Someone please help me implement it
+
+  # Unless cd to $HOME, add path to history
+  string match -q $HOME $new_path || set new_history $new_path $new_history
+  echo $new_history
 end
 
 function cd --description "Wrapper function for change directory"
+  set -l cd_destination ""
   set -l cd_status 0
+  set -l HISTORY_MEM_SIZE 25
 
   # Init cd_history in new shell
-  set -q cd_history; or set -g cd_history
+  set -q cd_history || set -g cd_history
+  set -q cd_prev_path || set -g cd_prev_path $HOME
   # Parse arguments
   set -l options (fish_opt -s l -l list)
   set -a options (fish_opt -s i -l index --required-val)
@@ -38,53 +41,45 @@ function cd --description "Wrapper function for change directory"
 
   # Check if using 2 options
   if set -q _flag_index; and set -q _flag_list
-    echo "cd: Only use one option at a time"
+    echo "ERROR: Only use one option at a time"
     return 1
   end
 
   if set -q _flag_list        # Using list option
-    test (count $argv) -gt 0; and echo "cd: Print dir history only, ignore other arguments"
+    test (count $argv) -gt 0 && echo "WARN: Print dir history only, ignore other arguments"
     set -l index 1
-    for dir in (string split ' ' $cd_history)
+    for dir in $cd_history
       echo "$index - $dir"
       set index (math $index + 1)
     end
     return 0
   else if set -q _flag_index  # If using index option
-    set _flag_index (string trim -c ' ' $_flag_index)
     if not __cd_int_check $_flag_index
-      echo "cd: $_flag_index is not an integer"
+      echo "ERROR: $_flag_index is not an integer"
       return 1
     end
-    if test $_flag_index -lt 1; or test $_flag_index -gt (count (string split ' ' $cd_history))
-      echo "cd: Invalid history index"
+    if test $_flag_index -lt 1 -o $_flag_index -gt (count $cd_history)
+      echo "ERROR: Invalid history index"
       return 1
     end
-    test (count $argv) -gt 0; and echo "cd: Change directory to index $_flag_index, ignore other arguments"
-    set -l list_dir (string split ' ' $cd_history)
-    set argv $list_dir[$_flag_index]
+    test (count $argv) -gt 0 && echo "WARN: Change directory to index $_flag_index, ignore other arguments"
+    set cd_destination $cd_history[$_flag_index]
   else # default case
-    if not test "$argv" = '-'
-      set argv (__cd_explicit_path "$argv")
+    if test "$argv" = '-'
+      set cd_destination $cd_prev_path
     else
-      builtin cd -
-      set cd_status $status
-      if test $cd_status -eq 0
-        set -l dir (pwd)
-        set cd_history (__cd_remove_existed "$dir" "$cd_history")
-        set cd_history "$dir $cd_history"
-        set cd_history (string trim -c ' ' $cd_history)
-      end
-      return $cd_status
+      set cd_destination (__cd_explicit_path "$argv")
     end
   end
 
-  builtin cd $argv
+  set -l tmp_path (pwd)
+  builtin cd $cd_destination
   set cd_status $status
   if test $cd_status -eq 0
-    set cd_history (__cd_remove_existed "$argv" "$cd_history")
-    set cd_history "$argv $cd_history"
-    set cd_history (string trim -c ' ' $cd_history)
+    # Save the previous path if new destination is different with the previous
+    string match -q "$tmp_path" "$cd_destination" || set cd_prev_path $tmp_path
+    set cd_history (__cd_add_history $cd_destination "$cd_history")
+    set cd_history (string split --no-empty ' ' "$cd_history")
   end
   return $cd_status
 end
