7cb3997fbd4e821bcd213f90c777a8ce0ff24fa8
Luu Hoang Kien <kien.luuhoang.arch@gmail.com>
2023-04-16 13:15:23 +0700

    Add custom completions

--- customs/user.fish
+++ customs/user.fish
@@ -1,6 +1,7 @@
 # User config
 set -l CUSTOMIZED_CONF $HOME/.config/fish/customs/conf.d 
 set -l CUSTOMIZED_FUNC $HOME/.config/fish/customs/functions
+set -l CUSTOMIZED_COMP $HOME/.config/fish/customs/completions
 
 if test -d $CUSTOMIZED_CONF
   for conf in $CUSTOMIZED_CONF/*.fish
@@ -14,3 +15,8 @@ if test -d $CUSTOMIZED_FUNC
   end
 end
 
+if test -d $CUSTOMIZED_COMP
+  if not contains $CUSTOMIZED_COMP $fish_complete_path
+    set -a fish_complete_path $CUSTOMIZED_COMP
+  end
+end
